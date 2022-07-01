ad35cb02b4b373af87e6987868d29270df13ccf4
Kohei Terakawa <odayakalife@teraka.local>
2022-07-01 14:21:58 +0900

    add restcontroller

--- todo/src/main/java/jp/kobespiral/odajin/todo/service/ToDoService.java
+++ todo/src/main/java/jp/kobespiral/odajin/todo/service/ToDoService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import jp.kobespiral.odajin.todo.dto.ToDoForm;
 import jp.kobespiral.odajin.todo.entity.ToDo;
 import jp.kobespiral.odajin.todo.exception.ToDoAppException;
+import jp.kobespiral.odajin.todo.repository.MemberRepository;
 import jp.kobespiral.odajin.todo.repository.ToDoRepository;
 
 @Service
@@ -17,49 +18,71 @@ public class ToDoService {
     @Autowired
     ToDoRepository tRepo;
 
-    public ToDo createToDo(String mid,ToDoForm form){
+    @Autowired
+    MemberRepository mRepo;
 
-    //title,作成者，済マーク，作成日時，完了日時を入れたToDoエンティティを作成
-    ToDo t = new ToDo();
-    t.setMid(mid);
+    public ToDo createToDo(String mid, ToDoForm form) {
 
-    t.setDone(false);
-    t.setCreatedAt(new Date());
-    t.setTitle(form.getTitle());
+        // title,作成者，済マーク，作成日時，完了日時を入れたToDoエンティティを作成
+        ToDo t = new ToDo();
+        t.setMid(mid);
 
-    return tRepo.save(t);
+        t.setDone(false);
+        t.setCreatedAt(new Date());
+        t.setTitle(form.getTitle());
+
+        return tRepo.save(t);
     }
+
     public ToDo getToDo(Long seq) {
         ToDo t = tRepo.findById(seq).orElseThrow(
                 () -> new ToDoAppException(ToDoAppException.NO_SUCH_TODO_EXISTS, seq + ": No such todo exists"));
         return t;
     }
 
-    //midのToDoリストを取得
+    // midのToDoリストを取得
     public List<ToDo> getToDoList(String mid) {
         return tRepo.findByMidAndDone(mid, false);
     }
 
-    //midのDoneリストを取得
+    // midのDoneリストを取得
     public List<ToDo> getDoneList(String mid) {
         return tRepo.findByMidAndDone(mid, true);
     }
 
-    //midのもつID:seqのToDoについて，完了済とする
-    public ToDo doneToDo(String mid,Long seq){
-        ToDo t = tRepo.findByMidAndSeq(mid,seq);
+    // midのもつID:seqのToDoについて，完了済とする
+    public ToDo doneToDo(String mid, Long seq) {
+        ToDo t = tRepo.findByMidAndSeq(mid, seq);
         t.setDone(true);
         t.setDoneAt(new Date());
         return tRepo.save(t);
     }
 
-    //全員のToDoリストを取得
-    public List<ToDo> getToDoList(){
+    // 全員のToDoリストを取得
+    public List<ToDo> getToDoList() {
         return tRepo.findByDone(false);
     }
 
-    //全員のDoneリストを取得
-    public List<ToDo> getDoneList(){
+    // 全員のDoneリストを取得
+    public List<ToDo> getDoneList() {
         return tRepo.findByDone(true);
+    }
+
+    // ToDoを更新する
+    public ToDo updateToDo(String mid, Long seq, ToDoForm form) {
+        ToDo t = tRepo.findByMidAndSeq(mid, seq);
+        t.setTitle(form.getTitle());
+        return tRepo.save(t);
+    }
+
+    // ToDoを削除する
+    public boolean deleteToDo(String mid, Long seq) {
+        ToDo t = tRepo.findByMidAndSeq(mid, seq);
+        if (t != null) {
+            tRepo.delete(t);
+            return true;
+        } else
+            return false;
+
     }
 }
